16814804a669c684743e32c8c3dcca0d18392a1c
zaklance <wosewick@gmail.com>
2024-07-05 14:46:22 -0400

    voila

--- client/src/components/Markets.jsx
+++ client/src/components/Markets.jsx
@@ -1,17 +1,22 @@
 import react from 'react';
 import MarketCard from './MarketCard';
 import { Map, Marker } from 'mapkit-react';
-import REACT_MAP_KEY from '../../.env'
 
 function Markets() {
     return (
         <>
             <header>FIND A FARMERS MARKET TODAY</header>
             <div id='map'>
-                <Map token=REACT_MAP_KEY>
+                <h2>Please Work</h2>
+                <Map token={import.meta.env.REACT_MAP_KEY} >
                     <Marker latitude={46.52} longitude={6.57} />
                 </Map>
             </div >
+            <script src="https://cdn.apple-mapkit.com/mk/5.x.x/mapkit.core.js"
+                crossOrigin='async'
+                data-callback="initMapKit"
+                data-libraries="services,full-map,geojson">
+            </script>
         </>
     )
 }
