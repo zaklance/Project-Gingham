96b0498709ce312740041dd3c34b750019e18ea8
Alessandro Ledesma <sandroledesma@gmail.com>
2024-07-05 14:39:33 -0400

    update files

--- client/src/components/Markets.jsx
+++ client/src/components/Markets.jsx
@@ -1,18 +1,35 @@
 import react from 'react';
 import MarketCard from './MarketCard';
 import { Map, Marker } from 'mapkit-react';
-import REACT_MAP_KEY from '../../.env'
+import '../assets/css/index.css';
+// import REACT_MAP_KEY from '../../.env';
 
 function Markets() {
+    const [ market, setMarket] = useState([]);
+    
+    useEffect(() => {
+        fetch("http://localhost:3000/markets")
+        .then(response => response.json())
+        .then(data => setMarket(data))
+    }, []);
+
     return (
-        <>
+        <div className="markets-container">
+            <br/>
             <header>FIND A FARMERS MARKET TODAY</header>
-            <div id='map'>
-                <Map token=REACT_MAP_KEY>
-                    <Marker latitude={46.52} longitude={6.57} />
-                </Map>
-            </div >
-        </>
+            <h4>Click on the image to learn more</h4>
+                {/* <div id='map'>
+                    <Map token=REACT_MAP_KEY>
+                        <Marker latitude={46.52} longitude={6.57} />
+                    </Map>
+                </div > */}
+            <br/>
+            <div className="market-cards-container">
+                {market.map((marketData) => (
+                    <MarketCard key={marketData.id} marketData={marketData} />
+                ))}
+            </div>
+        </div>
     )
 }
 
