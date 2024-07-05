64b99ec212c1550f688c89fdfdaef5aad9d65482
zaklance <wosewick@gmail.com>
2024-07-05 11:25:14 -0400

    env created

--- client/src/components/Markets.jsx
+++ client/src/components/Markets.jsx
@@ -1,10 +1,17 @@
 import react from 'react';
 import MarketCard from './MarketCard';
+import { Map, Marker } from 'mapkit-react';
+import REACT_MAP_KEY from '../../.env'
 
 function Markets() {
     return (
         <>
             <header>FIND A FARMERS MARKET TODAY</header>
+            <div id='map'>
+                <Map token=REACT_MAP_KEY>
+                    <Marker latitude={46.52} longitude={6.57} />
+                </Map>
+            </div >
         </>
     )
 }
