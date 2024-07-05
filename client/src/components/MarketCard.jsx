ddacff3620ce838fb58f03ff47893cbf09e250f8
zaklance <wosewick@gmail.com>
2024-07-05 14:55:54 -0400

    "wierd"

--- client/src/components/MarketCard.jsx
+++ client/src/components/MarketCard.jsx
@@ -1,11 +1,16 @@
-import react from 'react';
+import React from 'react';
+import '../assets/css/index.css';
 
-
-function MarketCard() {
+function MarketCard({ marketData }) {
     return (
-        <>
-        </>
+        <div className="market-card">
+            <img src={marketData.image} alt="Market Image" style={{width: '260px'}}/>
+            <h2>{marketData.name}</h2>
+            <h4>{marketData.location}</h4>
+            <h4>{marketData.hours}</h4>
+            <button className="market-card-button">Learn More!</button>
+        </div>
     )
 }
 
-export default MarketCard;
+export default MarketCard; 
