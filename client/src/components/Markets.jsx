1eaac82c91641291679850b0f0772b3b6a553945
Alessandro Ledesma <sandroledesma@gmail.com>
2024-07-05 15:00:06 -0400

    comment!

--- client/src/components/Markets.jsx
+++ client/src/components/Markets.jsx
@@ -1,11 +1,8 @@
 import react from 'react';
 import MarketCard from './MarketCard';
 import { Map, Marker } from 'mapkit-react';
-<<<<<<< HEAD
-=======
 import '../assets/css/index.css';
 // import REACT_MAP_KEY from '../../.env';
->>>>>>> 99736edc5fdef732f8d914edb0e2f8a7217c09be
 
 function Markets() {
     const [ market, setMarket] = useState([]);
@@ -20,8 +17,8 @@ function Markets() {
         <div className="markets-container">
             <br/>
             <header>FIND A FARMERS MARKET TODAY</header>
-<<<<<<< HEAD
-            <div id='map'>
+            <h4>Click on the image to learn more</h4>
+            {/* <div id='map'>
                 <h2>Please Work</h2>
                 <Map token={import.meta.env.REACT_MAP_KEY} >
                     <Marker latitude={46.52} longitude={6.57} />
@@ -31,15 +28,7 @@ function Markets() {
                 crossOrigin='async'
                 data-callback="initMapKit"
                 data-libraries="services,full-map,geojson">
-            </script>
-        </>
-=======
-            <h4>Click on the image to learn more</h4>
-                {/* <div id='map'>
-                    <Map token=REACT_MAP_KEY>
-                        <Marker latitude={46.52} longitude={6.57} />
-                    </Map>
-                </div > */}
+            </script> */}
             <br/>
             <div className="market-cards-container">
                 {market.map((marketData) => (
@@ -47,7 +36,6 @@ function Markets() {
                 ))}
             </div>
         </div>
->>>>>>> 99736edc5fdef732f8d914edb0e2f8a7217c09be
     )
 }
 
