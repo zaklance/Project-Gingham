cb6a7dce85a5242f55e7604878235b99edfc8337
zaklance <wosewick@gmail.com>
2024-07-05 14:55:54 -0400

    "wierd"

--- client/src/components/Vendors.jsx
+++ client/src/components/Vendors.jsx
@@ -1,11 +1,29 @@
-import react from 'react';
-import VendorCard from './VendorCard.jsx';
-
+import React from 'react';
+import { useState, useEffect } from 'react';
+import VendorCard from './VendorCard';
+import '../assets/css/index.css';
 
 function Vendors() {
+    const [ vendor, setVendor] = useState([]);
+    
+    useEffect(() => {
+        fetch("http://localhost:3000/vendors")
+        .then(response => response.json())
+        .then(data => setVendor(data))
+    }, []);
+    
     return (
-        <>
-        </>
+        <div className="markets-container">
+            <br/>
+            <header>FIND A MARKET VENDOR TODAY</header>
+            <h4>Click on the image to learn more</h4>
+            <br/>
+            <div className="market-cards-container">
+                {vendor.map((vendorData) => (
+                    <VendorCard key={vendorData.id} vendorData={vendorData} />
+                ))}
+            </div>
+        </div>
     )
 }
 
