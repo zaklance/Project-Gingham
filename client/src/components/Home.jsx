9cca870f312b5b1851e57c63416f8683985d577e
zaklance <wosewick@gmail.com>
2024-07-08 12:12:16 -0400

    css mostly done

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import ginghamLogo from '../assets/images/gingham-4-alt_1200px.png';
+import farmers from '../assets/images/22bitman.xlarge1.jpg';
 
 function Home() {
     return (
         <div className="wrapper">
-            <img src={ginghamLogo} alt="Gingham Logo" style={{ width: '300px' }}></img>
-            <h1>Project Gingham</h1>
-            <p className="sidebar">
-                DO you love to cook? Do you love cheap produce? How about picnics in the park? Well here is the startup for you: Gingham. When vendors at farmers markets approach the end of the day, they want to start git rid of all their product, and sometimes at a cheaper price. With Gingham you can browse through vendors and buy discount bundles!
-            </p>
-            <h3> GET A FREE GINGHAM PICNIC BLANKET AFTER SIGN UP! </h3>
-            <p> **NEW CUSTOMERS ONLY** </p>
+            <img className='big-logo' src={ginghamLogo} alt="Gingham Logo"></img>
+            <div className="sidebar">
+                <h1>Project Gingham</h1>
+                <br></br>
+                <p>
+                    Do you love to cook? Do you love cheap produce? How about picnics in the park? Well here is the startup for you: Gingham. When vendors at farmers markets approach the end of the day, they want to start git rid of all their product, and sometimes at a cheaper price. With Gingham you can browse through vendors and buy discount bundles!
+                </p>
+                <br></br>
+                <h4> GET A FREE GINGHAM PICNIC BLANKET AFTER SIGN UP! </h4>
+                <p> **NEW CUSTOMERS ONLY** </p>
+            </div>
+            <div className="box-big">
+                <img src={farmers}/>
+            </div>
         </div>
     )
 }
