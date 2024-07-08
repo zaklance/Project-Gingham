7953c32f49a693cf7e80d8855edc5a0898d6c6ea
zaklance <wosewick@gmail.com>
2024-07-08 16:40:22 -0400

    vendor image changes

--- client/src/components/VendorDetail.jsx
+++ client/src/components/VendorDetail.jsx
@@ -5,20 +5,8 @@ const VendorDetail = () => {
     const { id } = useParams();
     const [vendor, setVendor] = useState(null);
     const [availableBaskets, setAvailableBaskets] = useState(5);
-    const [randomImage, setRandomImage] = useState('');
     const [marketDetails, setMarketDetails] = useState({});
     const [locations, setLocations] = useState([]); // Initialize as empty array
-
-    const images = [
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJdUQVqfYHV6YWtlJYuNouSOjUqHSEetAGSg&s',
-        'https://www.opkansas.org/wp-content/uploads/2019/06/opfm-vendor-web2.jpg',
-        'https://static.wixstatic.com/media/05bd2f_2b30b89b49eb4b2e95810360a9357bd2~mv2_d_7360_4912_s_4_2.jpeg/v1/fill/w_640,h_434,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/05bd2f_2b30b89b49eb4b2e95810360a9357bd2~mv2_d_7360_4912_s_4_2.jpeg',
-        'https://www.merriam.org/files/sharedassets/public/v/1/1.-photos/parks/farmers-market-1.jpg?dimension=pageimage&w=480',
-        'https://c8.alamy.com/comp/2R82FT1/st-jacobs-farmers-market-fruit-and-vegetable-vendors-ontario-canada-2R82FT1.jpg',
-        'https://www.fairburn.com/sites/default/files/uploads/ParksAndRecreation/document_2.jpg',
-        'https://www.lanecountyfarmersmarket.org/wp-content/uploads/2022/02/Vendor-Slider-3-scaled.jpg',
-        'https://frontierefarmhouse.wordpress.com/wp-content/uploads/2019/09/66422240_2392773677468030_9162452177778638848_o.jpg?w=1024'
-    ];
 
     useEffect(() => {
         fetch(`http://127.0.0.1:5555/vendors/${id}`)
@@ -26,8 +14,6 @@ const VendorDetail = () => {
             .then(data => {
                 setVendor(data);
                 setLocations(data.locations || []);
-                const randomIndex = Math.floor(Math.random() * images.length);
-                setRandomImage(images[randomIndex]);
             })
             .catch(error => console.error('Error fetching vendor data:', error));
     }, [id]);
@@ -79,7 +65,7 @@ const VendorDetail = () => {
         <div>
             <div>
                 <h2>{vendor.name}</h2>
-                <img src={randomImage} alt="Vendor Image" style={{ width: '70%' }} />
+                <img src={vendor.image} alt="Vendor Image" style={{ width: '70%' }} />
                 <h4>Based out of: {vendor.based_out_of}</h4>
                 <br />
                 <h4>Farmers Market Locations:</h4>
