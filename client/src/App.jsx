05405c334d9d9b1ff730f5c59e6498a98a0abeec
Alessandro Ledesma <sandroledesma@gmail.com>
2024-07-09 17:27:28 -0400

    commit?

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Outlet, useLoaderData } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 //CSS
 import './assets/css/index.css';
@@ -13,7 +13,7 @@ function App() {
         <>
             <div className="container">
                 <header> <NavBar /> </header>
-                <main> <Outlet context={[user]}/> </main>
+                <main> <Outlet /> </main>
             </div>
         </>
     )
