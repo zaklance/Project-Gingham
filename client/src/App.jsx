fbe5c7c1c4acdab7858a522b34a4492024588611
vhle12 <158373104+vhle12@users.noreply.github.com>
2024-07-11 11:16:10 -0400

    cart functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,37 @@
-import React from 'react';
+// App.jsx
+import React, { useState, useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
-
-//CSS
 import './assets/css/index.css';
-
-//Components
 import NavBar from './components/NavBar.jsx';
 
 function App() {
+    const [amountInCart, setAmountInCart] = useState(() => {
+        return parseInt(localStorage.getItem('amountInCart') || 0);
+    });
+
+    const [cartItems, setCartItems] = useState(() => {
+        const savedCartItems = localStorage.getItem('cartItems');
+        return savedCartItems ? JSON.parse(savedCartItems) : [];
+    });
+
+    useEffect(() => {
+        localStorage.setItem('amountInCart', amountInCart);
+    }, [amountInCart]);
+
+    useEffect(() => {
+        localStorage.setItem('cartItems', JSON.stringify(cartItems));
+    }, [cartItems]);
 
     return (
-        <>
-            <div className="container">
-                <header> <NavBar /> </header>
-                <main> <Outlet /> </main>
-            </div>
-        </>
-    )
+        <div className="container">
+            <header>
+                <NavBar />
+            </header>
+            <main>
+                <Outlet context={{ amountInCart, setAmountInCart, cartItems, setCartItems }} />
+            </main>
+        </div>
+    );
 }
 
 export default App;
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavLink, useLocation } from 'react-router-dom';
+import { NavLink, useLocation, useOutletContext } from 'react-router-dom';
 import ginghamLogo from '../assets/images/gingham-2-alt_1200px.png';
 import '../assets/css/index.css';
 
@@ -7,37 +7,52 @@ function NavBar() {
     const location = useLocation();
     const userId = localStorage.getItem('userId');
 
-    const isLoggedIn = userId && location.pathname !== '/' && location.pathname !== '/login';
 
+    const isLoggedIn = userId && location.pathname !== '/' && location.pathname !== '/login';
+    
     return (
         <nav className="nav-bar">
             <ul>
-                <img className='logo' src={ginghamLogo} alt="Gingham Logo"></img>
+                <img className='logo' src={ginghamLogo} alt="Gingham Logo" />
                 {isLoggedIn ? (
                     <>
                         <li>
-                            <button className='nav-tab color-2'><NavLink reloadDocument to={`/profile/${userId}`}>Profile</NavLink></button>
+                            <button className='nav-tab color-2'>
+                                <NavLink reloadDocument to={`/profile/${userId}`}>Profile</NavLink>
+                            </button>
                         </li>
                         <li>
-                            <button className='nav-tab color-3'><NavLink reloadDocument to="/markets">Markets</NavLink></button>
+                            <button className='nav-tab color-3'>
+                                <NavLink reloadDocument to="/markets">Markets</NavLink>
+                            </button>
                         </li>
                         <li>
-                            <button className='nav-tab color-4'><NavLink reloadDocument to="/vendors">Vendors</NavLink></button>
+                            <button className='nav-tab color-4'>
+                                <NavLink reloadDocument to="/vendors">Vendors</NavLink>
+                            </button>
                         </li>
                         <li>
-                            <button className='nav-tab color-5'><NavLink reloadDocument to="/cart">Cart</NavLink></button>
+                            <button className='nav-tab color-5'>
+                                <NavLink reloadDocument to="/cart">Cart</NavLink>
+                            </button>
                         </li>
                         <li style={{ marginLeft: 'auto' }}>
-                            <button className='nav-tab color-6 tab-right'><NavLink reloadDocument to="/logout">Logout</NavLink></button>
+                            <button className='nav-tab color-6 tab-right'>
+                                <NavLink reloadDocument to="/logout">Logout</NavLink>
+                            </button>
                         </li>
                     </>
                 ) : (
                     <>
                         <li>
-                            <button className='nav-tab color-1'><NavLink reloadDocument to="/">Home</NavLink></button>
+                            <button className='nav-tab color-1'>
+                                <NavLink reloadDocument to="/">Home</NavLink>
+                            </button>
                         </li>
                         <li style={{ marginLeft: 'auto' }}>
-                                <button className='nav-tab color-6 tab-right'><NavLink reloadDocument to="/login">Login/Signup</NavLink></button>
+                            <button className='nav-tab color-6 tab-right'>
+                                <NavLink reloadDocument to="/login">Login/Signup</NavLink>
+                            </button>
                         </li>
                     </>
                 )}
