3ecf0a0acc5553c13b424a635590385f855b3564
zaklance <wosewick@gmail.com>
2024-08-13 10:20:23 -0400

    checkout page changes, added arrow to selector

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -35,38 +35,40 @@ function Cart() {
 	})
 	
     return (
-        <div className="cart">
+        <div>
             <h2>Shopping Cart</h2>
             {cartItems.length === 0 ? (
                 <p>Your cart is empty.</p>
             ) : (
                 <>
-                    <ul>
-                        {cartItems.map((item, index) => (
-                            <li className='cart-item' key={index}>
-                                <span><b>{item.vendorName}</b> at {item.location} {item.price}</span>
-                                <button className='btn-cart' onClick={() => removeFromCart(item)}>Remove</button>
-                            </li>
-                        ))}
-                    </ul>
-                    <div>
-					<h3>Total: {totalPrice}</h3> 
-                    </div>
-                    <div className="checkout-form">
-                        <h3>Checkout</h3>
-                        <input
-                            type="text"
-                            placeholder="Name"
-                            value={name}
-                            onChange={(e) => setName(e.target.value)}
-                            />
-                        <input
-                            type="text"
-                            placeholder="Address"
-                            value={address}
-                            onChange={(e) => setAddress(e.target.value)}
-                            />
-                        <button className='btn-cart' onClick={handleCheckout}>Checkout</button>
+                    <div className='checkout'>
+                        <div className="cart">
+                            <ul>
+                                {cartItems.map((item, index) => (
+                                    <li className='cart-item' key={index}>
+                                        <span><b>{item.vendorName}</b> at {item.location} {item.price}</span>
+                                        <button className='btn-cart' onClick={() => removeFromCart(item)}>Remove</button>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                        <div className="checkout-form">
+                            <h3>Checkout</h3>
+                            <h3>Total: {totalPrice}</h3> 
+                            <input
+                                type="text"
+                                placeholder="Name"
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
+                                />
+                            <input
+                                type="text"
+                                placeholder="Address"
+                                value={address}
+                                onChange={(e) => setAddress(e.target.value)}
+                                />
+                            <button className='btn-cart' onClick={handleCheckout}>Checkout</button>
+                        </div>
                     </div>
                 </>
             )}
