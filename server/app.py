ff954102d8d9e8a675e5f686fc71ab4ecf38dd21
zaklance <wosewick@gmail.com>
2024-09-19 08:56:43 -0400

    removed all references to favorites

--- server/app.py
+++ server/app.py
@@ -149,8 +149,8 @@ def profile(id):
     if request.method == 'GET':
         profile_data = user.to_dict()
     
-        profile_data['vendor_favorites'] = json.loads(user.vendor_favorites) if user.vendor_favorites else []
-        profile_data['market_favorites'] = json.loads(user.market_favorites) if user.market_favorites else []
+        # profile_data['vendor_favorites'] = json.loads(user.vendor_favorites) if user.vendor_favorites else []
+        # profile_data['market_favorites'] = json.loads(user.market_favorites) if user.market_favorites else []
         return jsonify(profile_data), 200
 
     elif request.method == 'PATCH':
