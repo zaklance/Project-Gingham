91d3c4945ebbb7e62078ec3f4c8732c7ccaea91b
Alessandro Ledesma <sandroledesma@gmail.com>
2024-10-19 13:05:51 -0400

    some debugging

--- client/src/components/user/VendorCard.jsx
+++ client/src/components/user/VendorCard.jsx
@@ -13,7 +13,7 @@ function VendorCard({ vendorData }) {
         <div className="market-card">
             <img src={vendorData.image} alt="Vendor Image" style={{width: '260px'}}/>
             <h2>{vendorData.name}</h2>
-            <h4>{vendorData.based_out_of}</h4>
+            <h4>{vendorData.city}, {vendorData.state}</h4>
             <h4>{vendorData.product}</h4>
             <button className="market-card-button" onClick={handleLearnMore}>Buy a Farmers Market Basket!</button>
         </div>
