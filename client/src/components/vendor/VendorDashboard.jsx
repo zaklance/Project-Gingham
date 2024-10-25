34b5d5e1de54ce97d0cf43bdb3821ff0c02a09bc
Alessandro Ledesma <sandroledesma@gmail.com>
2024-10-25 14:16:30 -0400

    update MarketCard and MarketDetail

--- client/src/components/vendor/VendorDashboard.jsx
+++ client/src/components/vendor/VendorDashboard.jsx
@@ -10,27 +10,27 @@ function VendorDashboard() {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
-    useEffect(() => {
-        const fetchVendorData = async () => {
-            try {
-                const response = await fetch(`http://127.0.0.1:5555/vendors/${id}`);
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                const data = await response.json();
-                const parsedLocations = JSON.parse(data.locations);
-                setLocations(parsedLocations);
-                setAvailableBaskets(parsedLocations.reduce((acc, loc) => ({ ...acc, [loc]: 5 }), {})); // Set initial baskets
-                setPrice(parsedLocations.reduce((acc, loc) => ({ ...acc, [loc]: 4.99 }), {})); // Set initial prices
-            } catch (error) {
-                console.error('Error fetching vendor data:', error);
-                setError(error.message);
-            } finally {
-                setLoading(false);
-            }
-        };
-        fetchVendorData();
-    }, [id]);
+    // useEffect(() => {
+    //     const fetchVendorData = async () => {
+    //         try {
+    //             const response = await fetch(`http://127.0.0.1:5555/vendors/${id}`);
+    //             if (!response.ok) {
+    //                 throw new Error('Network response was not ok');
+    //             }
+    //             const data = await response.json();
+    //             const parsedLocations = JSON.parse(data.locations);
+    //             setLocations(parsedLocations);
+    //             setAvailableBaskets(parsedLocations.reduce((acc, loc) => ({ ...acc, [loc]: 5 }), {})); // Set initial baskets
+    //             setPrice(parsedLocations.reduce((acc, loc) => ({ ...acc, [loc]: 4.99 }), {})); // Set initial prices
+    //         } catch (error) {
+    //             console.error('Error fetching vendor data:', error);
+    //             setError(error.message);
+    //         } finally {
+    //             setLoading(false);
+    //         }
+    //     };
+    //     fetchVendorData();
+    // }, [id]);
 
     useEffect(() => {
         if (locations.length > 0) {
