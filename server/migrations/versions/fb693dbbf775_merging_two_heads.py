748a09f9f3154d6f86b7e9b3a0adf18c5e843e26
zaklance <wosewick@gmail.com>
2024-11-04 10:10:10 -0500

    add schedule to seed/models, ignore redundant markets

---
+++ server/migrations/versions/fb693dbbf775_merging_two_heads.py
@@ -0,0 +1,24 @@
+"""merging two heads
+
+Revision ID: fb693dbbf775
+Revises: 9b5f14fd3db7, bab735ebf207
+Create Date: 2024-11-04 09:54:33.410614
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'fb693dbbf775'
+down_revision = ('9b5f14fd3db7', 'bab735ebf207')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
