6afada24c83d0e5a45ef367b4541648e2e589609
zaklance <wosewick@gmail.com>
2024-11-16 09:55:39 -0500

    VendorDetails vendor fetch fixed

--- client/src/components/admin/AdminVendors.jsx
+++ client/src/components/admin/AdminVendors.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function AdminVendors () {
+    const [vendors, setVendors] = useState([]);
+
+
+    useEffect(() => {
+        fetch("http://127.0.0.1:5555/markets")
+            .then(response => response.json())
+            .then(markets => setVendors(markets))
+            .catch(error => console.error('Error fetching markets', error));
+    }, []);
+
+
     return(
         <div>
             <h2 className='margin-t-16'>Vendor Management</h2>
-            <div className='bounding-box'>
+            <div className='bounding-box margin-r-24'>
                 <p>**vendor onboard, emails, etc goes here**</p>
             </div>
         </div>
