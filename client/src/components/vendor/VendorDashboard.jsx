1cfc16e148dccc5625d4038588b668bfaf20c0dd
zaklance <wosewick@gmail.com>
2024-11-21 11:33:15 -0500

    VendorDashboard reshuffle, add VendorEvents

--- client/src/components/vendor/VendorDashboard.jsx
+++ client/src/components/vendor/VendorDashboard.jsx
@@ -1,27 +1,26 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import VendorBasketCard from './VendorBasketCard';
-import VendorCreate from './VendorCreate';
+import { NavLink, Link } from 'react-router-dom';
 import VendorNotification from './VendorNotification';
+import VendorBaskets from './VendorBaskets';
+import VendorEvents from './VendorEvents';
 
-function VendorDashboard( {vendorId, marketId}) {
-    const [vendorUserData, setVendorUserData] = useState(null);
+function VendorDashboard({ vendorId, marketId }) {
+    const [vendors, setVendors] = useState([]);
+    const [activeTab, setActiveTab] = useState('baskets');
     const [notifications, setNotifications] = useState([]);
+    const [vendorUserData, setVendorUserData] = useState(null);
     const [newVendor, setNewVendor] = useState(false);
-    const [locations, setLocations] = useState([]);
-    const [marketDetails, setMarketDetails] = useState({});
-    const [availableBaskets, setAvailableBaskets] = useState({});
-    const [price, setPrice] = useState({});
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
 
-    const vendorUserId = sessionStorage.getItem('vendor_user_id');
-    const marketPrice = price[marketId] !== undefined ? price[marketId] : 0;
-    const marketBaskets = availableBaskets[marketId] !== undefined ? availableBaskets[marketId] : 0;
+    useEffect(() => {
+        fetch("http://127.0.0.1:5555/api/vendors")
+            .then(response => response.json())
+            .then(vendors => setVendors(vendors))
+            .catch(error => console.error('Error fetching vendors', error));
+    }, []);
 
     useEffect(() => {
         const fetchVendorUserData = async () => {
-            const vendorUserId = sessionStorage.getItem('vendor_user_id'); 
+            const vendorUserId = sessionStorage.getItem('vendor_user_id');
             if (!vendorUserId) {
                 console.error("No vendor user ID found");
                 return;
@@ -57,7 +56,8 @@ function VendorDashboard( {vendorId, marketId}) {
             }
         };
         fetchVendorUserData();
-    }, []); 
+    }, []);
+
 
     useEffect(() => {
         if (!vendorId) return;
@@ -97,157 +97,24 @@ function VendorDashboard( {vendorId, marketId}) {
         fetchNotifications();
     }, [vendorId]); 
 
-    // useEffect(() => {
-    //     const fetchVendorData = async () => {
-    //         try {
-    //             const response = await fetch(`http://127.0.0.1:5555/api/vendors/${id}`);
-    //             if (!response.ok) {
-    //                 throw new Error('Network response was not ok');
-    //             }
-    //             const data = await response.json();
-    //             const parsedLocations = JSON.parse(data.locations);
-    //             setLocations(parsedLocations);
-    //             setAvailableBaskets(parsedLocations.reduce((acc, loc) => ({ ...acc, [loc]: 5 }), {})); // Set initial baskets
-    //             setPrice(parsedLocations.reduce((acc, loc) => ({ ...acc, [loc]: 4.99 }), {})); // Set initial prices
-    //         } catch (error) {
-    //             console.error('Error fetching vendor data:', error);
-    //             setError(error.message);
-    //         } finally {
-    //             setLoading(false);
-    //         }
-    //     };
-    //     fetchVendorData();
-    // }, [id]);
-
-    // useEffect(() => {
-    //     if (locations.length > 0) {
-    //         const fetchMarketDetails = async () => {
-    //             const promises = locations.map(async (marketId) => {
-    //                 try {
-    //                     const response = await fetch(`http://127.0.0.1:5555/api/markets/${marketId}`);
-    //                     if (response.ok) {
-    //                         const marketData = await response.json();
-    //                         return { 
-    //                             id: marketId, 
-    //                             name: marketData.name
-    //                         };
-    //                     } else {
-    //                         console.log(`Failed to fetch market ${marketId}`);
-    //                         return { id: marketId, name: 'Unknown Market' };
-    //                     }
-    //                 } catch (error) {
-    //                     console.error(`Error fetching market ${marketId}:`, error);
-    //                     return { id: marketId, name: 'Unknown Market' };
-    //                 }
-    //             });
-
-    //             const details = await Promise.all(promises);
-    //             const marketDetailsMap = {};
-    //             details.forEach(detail => {
-    //                 marketDetailsMap[detail.id] = detail;
-    //             });
-    //             setMarketDetails(marketDetailsMap);
-    //         };
-
-    //         fetchMarketDetails();
-    //     }
-    // }, [locations]);
-
-    // const handleChange = (marketId, type, value) => {
-    //     if (type === 'price') {
-    //         setPrice(prevPrices => ({
-    //             ...prevPrices,
-    //             [marketId]: value
-    //         }));
-    //     } else if (type === 'baskets') {
-    //         setAvailableBaskets(prevBaskets => ({
-    //             ...prevBaskets,
-    //             [marketId]: value
-    //         }));
-    //     }
-    // };
-
-    // const handleSaveChanges = (marketId) => {
-    //     console.log(`Saving changes for market ${marketId}: Price: $${price[marketId].toFixed(2)}, Available Baskets: ${availableBaskets[marketId]}`);
-    // };
-
-    // if (loading) {
-    //     return <div>Loading...</div>;
-    // }
-
-    // if (error) {
-    //     return <div>Error: {error}</div>;
-    // }
-
     return (
         <div>
             <h2 className='margin-t-16'>Vendor Dashboard</h2>
-            {/* {notifications.length > 0 && */}
+            {notifications.length > 0 &&
                 <div className='box-bounding'>
                     <VendorNotification notifications={notifications} />
                 </div>
-            {/* } */}
-
-            {!vendorUserData || !vendorUserData.vendor_id ? (
-                <div className='box-bounding'>
-                    <VendorCreate />
-                </div>
-            ) : (
-                <div className='box-bounding'>
-                    <h3>Todays Markets:</h3>
-                    <div className='market-cards-container'>
-                        <div className='market-card'>
-                            <h3><strong>Union Square Market</strong></h3>
-                            <h4>April 9, Wednesday</h4>
-                            <br />
-                            <p>Available Baskets: 5</p>
-                            <br />
-                            <p>Pick Up Time: </p>
-                            <p>04:30 PM (1 hour)</p>
-                            <br />
-                            <p><strong>Claimed Baskets: 3</strong></p>
-                            <div className='box-bounding'>
-                                <p>sandroledesma, 04:45 PM<strong> ✓ </strong></p>
-                                <p>zaklance, 05:15 PM</p>
-                                <p>vhle, 05:15 PM</p>
-                            </div>
-                        </div>
-                    </div>
-                    <br/>
-                        {/* {locations.map((marketId) => (
-                            <div key={marketId} className='market-item'>
-                                <h3>{marketDetails[marketId]?.name || 'Loading...'}</h3>
-                                <div className='market-info'>
-                                    <label>
-                                        Price: 
-                                        <input 
-                                            type='number' 
-                                            value={marketPrice}
-                                            onChange={(e) => handleChange(marketId, 'price', parseFloat(e.target.value))}
-                                        />
-                                    </label>
-                                    <label>
-                                        Available Baskets: 
-                                        <input 
-                                            type='number' 
-                                            value={marketBaskets}
-                                            onChange={(e) => handleChange(marketId, 'baskets', parseInt(e.target.value))}
-                                        />
-                                    </label>
-                                    <button className='btn-edit' onClick={() => handleSaveChanges(marketId)}>Save Changes</button>
-                                </div>
-                            </div>
-                        ))} */}
-                    <h3>Future Markets:</h3>
-                    <p>Edits can be made until 9AM the day of the market unless basket has already been claimed by customer</p>
-                    <br/>
-                    <div className='market-cards-container'>
-                        <VendorBasketCard vendorId={vendorId}/>
-                        <VendorBasketCard />
-                        <VendorBasketCard />
-                    </div>
-                </div>
-            )}
+            }
+            <div className='tabs margin-t-20'>
+                <Link to="#" onClick={() => setActiveTab('baskets')} className={activeTab === 'baskets' ? 'active-tab btn btn-reset btn-tab margin-r-24' : 'btn btn-reset btn-tab margin-r-24'}>
+                    Baskets
+                </Link>
+                <Link to="#" onClick={() => setActiveTab('events')} className={activeTab === 'events' ? 'active-tab btn btn-reset btn-tab' : 'btn btn-reset btn-tab'}>
+                    Events
+                </Link>
+            </div>
+            {activeTab === 'baskets' && <VendorBaskets marketId={marketId} vendorId={vendorId} vendorUserData={vendorUserData} newVendor={newVendor} setNewVendor={setNewVendor} />}
+            {activeTab === 'events' && <VendorEvents vendors={vendors} vendorId={vendorId} vendorUserData={vendorUserData} />}
         </div>
     );
 }
---
+++ client/src/components/vendor/VendorEvents.jsx
@@ -0,0 +1,269 @@
+import React, { useState, useEffect } from 'react';
+
+function VendorEvents({ vendors, vendorId, vendorUserData }) {
+    const [newEvent, setNewEvent] = useState({});
+    const [events, setEvents] = useState([]);
+    const [editingEventId, setEditingEventId] = useState(null);
+    const [editedEventData, setEditedEventData] = useState({});
+
+    console.log()
+
+    useEffect(() => {
+        setNewEvent((prevEvent) => ({
+            ...prevEvent,
+            vendor_id: vendorUserData.id,
+        }));
+    }, [vendorUserData.id]);
+
+    const handleInputEventChange = (event) => {
+        setNewEvent({
+            ...newEvent,
+            [event.target.name]: event.target.value,
+        });
+    };
+
+    const handleEditInputChange = (event) => {
+        const { name, value } = event.target;
+        setEditedEventData((prev) => ({
+            ...prev,
+            [name]: value,
+        }));
+    };
+
+    const handleEventEditToggle = (eventId, title, message, start_date, end_date) => {
+        setEditingEventId(eventId);
+        setEditedEventData({ title, message, start_date, end_date });
+    };
+
+    const handleSaveNewEvent = async () => {
+        try {
+            console.log(newEvent);
+            // Save market details first
+            const response = await fetch(`http://127.0.0.1:5555/api/events`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(newEvent),
+            });
+
+            if (response.ok) {
+                const createdEvent = await response.json();
+                console.log('Event data updated successfully:', createdEvent);
+                alert('Market Event successfully created')
+                setEvents((prevEvents) => [...prevEvents, createdEvent]);
+                setNewEvent({})
+            } else {
+                console.log('Failed to save event details');
+                console.log('Response status:', response.status);
+                console.log('Response text:', await response.text());
+            }
+        } catch (error) {
+            console.error('Error saving event details:', error);
+        }
+    };
+
+    useEffect(() => {
+        fetch("http://127.0.0.1:5555/api/events")
+            .then(response => response.json())
+            .then(data => {
+                const today = new Date();
+                const sevenDaysFromNow = new Date();
+                sevenDaysFromNow.setDate(today.getDate() + 30);
+
+                const filteredData = data.filter(item => {
+                    const startDate = new Date(item.start_date);
+                    const endDate = new Date(item.end_date);
+                    return item.vendor_id === Number(vendorUserData.id) &&
+                        // Check if today is within range or start_date is within 7 days from now
+                        (today >= startDate && today <= endDate || startDate <= sevenDaysFromNow);
+                });
+                setEvents(filteredData);
+            })
+            .catch(error => console.error('Error fetching events', error));
+    }, [vendorUserData.id]);
+
+    const handleEventUpdate = async (eventId) => {
+        try {
+            const response = await fetch(`http://127.0.0.1:5555/api/events/${eventId}`, {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(editedEventData),
+            });
+
+            if (response.ok) {
+                const updatedEvent = await response.json();
+                setEvents((prevEvents) =>
+                    prevEvents.map((event) => (event.id === eventId ? updatedEvent : event))
+                );
+                setEditingEventId(null);
+                console.log('Event updated successfully:', updatedEvent);
+            } else {
+                console.error('Failed to update event:', await response.text());
+            }
+        } catch (error) {
+            console.error('Error updating event:', error);
+        }
+    };
+
+    const handleEventDelete = async (eventId) => {
+        try {
+
+            fetch(`http://127.0.0.1:5555/api/events/${eventId}`, {
+                method: "DELETE",
+            }).then(() => {
+                setEvents((prevEvents) => prevEvents.filter((review) => review.id !== eventId))
+            })
+        } catch (error) {
+            console.error("Error deleting review", error)
+        }
+    }
+
+    return (
+        <>
+            <div className='box-bounding'>
+                <h2>Add Events</h2>
+                <div className='margin-t-24'>
+                    <div className='form-group'>
+                        <label>Title:</label>
+                        <input
+                            type="text"
+                            name="title"
+                            placeholder='Holiday Market'
+                            value={newEvent.title || ''}
+                            onChange={handleInputEventChange}
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <label>Message:</label>
+                        <textarea
+                            className='textarea-edit'
+                            type="text"
+                            name="message"
+                            placeholder='Description here'
+                            value={newEvent.message || ''}
+                            onChange={handleInputEventChange}
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <label>Market ID:</label>
+                        <input
+                            type="text"
+                            name="vendor_id"
+                            placeholder='Search vendors above'
+                            value={vendorUserData.id || ''}
+                            readOnly
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <label title="yyyy-mm-dd">Event Start:</label>
+                        <input
+                            type="text"
+                            name="start_date"
+                            placeholder='yyyy-mm-dd'
+                            value={newEvent.start_date || ''}
+                            onChange={handleInputEventChange}
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <label title="yyyy-mm-dd">Event End:</label>
+                        <input
+                            type="text"
+                            name="end_date"
+                            placeholder='yyyy-mm-dd'
+                            value={newEvent.end_date || ''}
+                            onChange={handleInputEventChange}
+                        />
+                    </div>
+                    <button className='btn-edit' onClick={handleSaveNewEvent}>Create Event</button>
+                </div>
+            </div>
+            <div className='box-bounding'>
+                <h2>Edit Events</h2>
+                <div className='flex-wrap'>
+                    {events.length > 0 ? (
+                        events.map((event, index) => (
+                            <div key={index} style={{ borderBottom: '1px solid #ccc', padding: '8px 0' }}>
+                                {editingEventId === event.id ? (
+                                    <>
+                                        <div>
+                                            <div className='form-group'>
+                                                <label>Title:</label>
+                                                <input
+                                                    type="text"
+                                                    name="title"
+                                                    placeholder='Holiday Market'
+                                                    value={editedEventData ? editedEventData.title : ''}
+                                                    onChange={handleEditInputChange}
+                                                />
+                                            </div>
+                                            <div className='form-group'>
+                                                <label>Message:</label>
+                                                <textarea
+                                                    className='textarea-edit'
+                                                    type="text"
+                                                    name="message"
+                                                    placeholder='Description here'
+                                                    value={editedEventData ? editedEventData.message : ''}
+                                                    onChange={handleEditInputChange}
+                                                />
+                                            </div>
+                                            <div className='form-group'>
+                                                <label title="yyyy-mm-dd">Event Start:</label>
+                                                <input
+                                                    type="text"
+                                                    name="start_date"
+                                                    placeholder='yyyy-mm-dd'
+                                                    value={editedEventData ? editedEventData.start_date : ''}
+                                                    onChange={handleEditInputChange}
+                                                />
+                                            </div>
+                                            <div className='form-group'>
+                                                <label title="yyyy-mm-dd">Event End:</label>
+                                                <input
+                                                    type="text"
+                                                    name="end_date"
+                                                    placeholder='yyyy-mm-dd'
+                                                    value={editedEventData ? editedEventData.end_date : ''}
+                                                    onChange={handleEditInputChange}
+                                                />
+                                            </div>
+                                            <button className='btn btn-small margin-t-24' onClick={() => handleEventUpdate(event.id)}>Save</button>
+                                            <button className='btn btn-small btn-gap' onClick={() => setEditingEventId(null)}>Cancel</button>
+
+                                        </div>
+                                    </>
+                                ) : (
+                                    <>
+                                        <div className='flex-start flex-center-align flex-gap-16'>
+                                            <p className='text-italic nowrap'>
+                                                {event.start_date}
+                                                {event.end_date !== event.start_date && ` - `}
+                                                <br></br>
+                                                {event.end_date !== event.start_date && `${event.end_date}`}
+                                            </p>
+                                            <h3 className='nowrap'>{event.title ? event.title : 'Loading...'}:</h3>
+                                            <p>{event.message}</p>
+                                        </div>
+                                        <button className='btn btn-small' onClick={() => handleEventEditToggle(event.id, event.title, event.message, event.start_date, event.end_date)}>
+                                            Edit
+                                        </button>
+                                        <button className='btn btn-small btn-x btn-gap' onClick={() => handleEventDelete(event.id)}>
+                                            Delete
+                                        </button>
+                                    </>
+                                )}
+                            </div>
+                        ))
+                    ) : (
+                        <>
+                        </>
+                    )}
+                </div>
+            </div>
+        </>
+    )
+}
+export default VendorEvents;
