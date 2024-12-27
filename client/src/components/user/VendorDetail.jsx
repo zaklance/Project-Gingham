8b19f074b42f7cb58f0bba84d61256f8610dc9c8
zaklance <wosewick@gmail.com>
2024-12-27 11:11:53 -0500

    market random image, and filter btn

--- client/src/components/user/VendorDetail.jsx
+++ client/src/components/user/VendorDetail.jsx
@@ -288,6 +288,17 @@ function VendorDetail({ products }) {
         return <div>Loading...</div>;
     }
 
+    const randomImage = [
+        "vendor-default-1_1600px.png",
+        "vendor-default-2_1600px.png",
+        "vendor-default-3_1600px.png"
+    ]
+
+    const vendorImage = vendor.image
+        ? `/vendor-images/${vendor.image}`
+        : `/vendor-images/_default-images/${randomImage[Math.floor(Math.random() * randomImage.length)]}`;
+
+
     return (
         <div>
             <div className='flex-space-between'>
@@ -328,7 +339,7 @@ function VendorDetail({ products }) {
             </div>
             <div className='flex-space-between margin-t-24 flex-wrap'>
                 <div className='width-100'>
-                    <img className='img-vendor' src={`/vendor-images/${vendor.image}`} alt="Vendor Image"/>
+                    <img className='img-vendor' src={vendorImage} alt="Vendor Image"/>
                 </div>
                 <div className='side-basket'>
                     <h3 className='margin-t-8'>Product: {product ? product.product : ""}</h3>
