c6561204a2a60ff97077866fabeb1e165785764c
zaklance <wosewick@gmail.com>
2025-02-05 10:42:26 -0500

    add deploy branch

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:5555'
+        // target: 'http://127.0.0.1:5555'
+        target: 'https://gingham-nyc.onrender.com'
         // target: 'https://www.gingham.nyc'
       }
     }
