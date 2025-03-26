bd83da71d4da28a5c4fb440502dec623f7e4bd57
zaklance <wosewick@gmail.com>
2025-03-26 13:24:40 -0400

    add maintenance page

---
+++ client/src/components/Maintenance.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Maintenance() {
+    return (
+        <>
+            <div className='img-maintenance flex-center'>
+                <div className='box-maintenance'>
+                    <h1>Site Under Maintenance:</h1>
+                    <h1>Please Check Back Soon!</h1>
+                </div>
+                {/* <img className='img-maintenance' src="/market-images/_default-images/market-default-4_1600px.png"></img> */}
+
+            </div>
+        </>
+    );
+}
+
+export default Maintenance;
