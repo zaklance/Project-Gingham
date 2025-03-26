3c49c1b9fd4bd41ba70cdfea09b64cbc259b8eff
zaklance <wosewick@gmail.com>
2025-03-26 13:25:01 -0400

    add maintenance page

--- client/src/components/Maintenance.jsx
+++ client/src/components/Maintenance.jsx
@@ -8,8 +8,6 @@ function Maintenance() {
                     <h1>Site Under Maintenance:</h1>
                     <h1>Please Check Back Soon!</h1>
                 </div>
-                {/* <img className='img-maintenance' src="/market-images/_default-images/market-default-4_1600px.png"></img> */}
-
             </div>
         </>
     );
