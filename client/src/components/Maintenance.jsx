a937b6466334d0a39e4c6cd22a86a82e6263c317
zaklance <wosewick@gmail.com>
2025-03-27 12:16:16 -0400

    gingham font updates

--- client/src/components/Maintenance.jsx
+++ client/src/components/Maintenance.jsx
@@ -5,8 +5,11 @@ function Maintenance() {
         <>
             <div className='img-maintenance flex-center'>
                 <div className='box-maintenance'>
-                    <h1>Site Under Maintenance:</h1>
-                    <h1>Please Check Back Soon!</h1>
+                    <h1>
+                        Site Under Maintenance:
+                        <br />
+                        Please Check Back Soon!
+                    </h1>
                 </div>
             </div>
         </>
