24f8fde59c461cc0caa7177e8c7f4a063807769c
zaklance <wosewick@gmail.com>
2025-03-31 15:54:45 -0400

    redis celery first pass

---
+++ server/celery_config.py
@@ -0,0 +1,15 @@
+from celery import Celery
+import os
+
+def make_celery():
+    redis_url = os.getenv("REDIS_URL", "redis://localhost:6379/0")
+    # redis_url = "redis://localhost:6379/0"
+    # backend="redis://localhost:6379/1"
+    return Celery(
+        "tasks",
+        broker=redis_url,
+        backend=redis_url,
+        include=["tasks"]
+    )
+
+celery = make_celery()
