1127728d4f44bc9f928221f676ce0995d5a15c9c
zaklance <wosewick@gmail.com>
2025-04-01 11:50:57 -0400

    redis celery implementation

--- server/celery_config.py
+++ server/celery_config.py
@@ -2,8 +2,9 @@ from celery import Celery
 import os
 
 def make_celery():
-    redis_url = os.getenv("REDIS_URL", "redis://localhost:6379/0")
-    # redis_url = "redis://localhost:6379/0"
+    # redis_url = os.getenv("REDIS_URL", "redis://localhost:6379/0")
+    redis_url = "redis://localhost:6379/0"
+    # backend = os.getenv("DATABASE_URI", "redis://localhost:6379/0")
     # backend="redis://localhost:6379/1"
     return Celery(
         "tasks",
