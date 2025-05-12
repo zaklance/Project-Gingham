4f27733584ea0d98604a0c7e7b32fade16a7913b
zaklance <wosewick@gmail.com>
2025-05-12 13:43:48 -0400

    add ingredient edit, hide single email

--- client/src/components/admin/AdminRecipes.jsx
+++ client/src/components/admin/AdminRecipes.jsx
@@ -3,14 +3,13 @@ import { Link } from 'react-router-dom';
 import AdminRecipeAdd from './AdminRecipeAdd';
 import AdminRecipeEdit from './AdminRecipeEdit';
 import AdminRecipeDelete from './AdminRecipeDelete';
+import AdminRecipeIngredient from './AdminRecipeIngredient';
 
 const AdminRecipes = () => {
     const [recipes, setRecipes] = useState([]);
     const [smallwares, setSmallwares] = useState([]);
     const [ingredients, setIngredients] = useState([]);
     const [recipeIngredients, setRecipeIngredients] = useState([]);
-    const [instructionGroups, setInstructionGroups] = useState([]);
-    const [instructions, setInstructions] = useState([]);
     const [activeTab, setActiveTab] = useState('add');
     
     useEffect(() => {
@@ -47,20 +46,6 @@ const AdminRecipes = () => {
             .catch(error => console.error('Error fetching recipe ingredients', error));
     }, []);
 
-    useEffect(() => {
-        fetch('/api/instructions')
-            .then(response => response.json())
-            .then(data => setInstructions(data))
-            .catch(error => console.error('Error fetching instructions', error));
-    }, []);
-
-    useEffect(() => {
-        fetch('/api/instruction-groups')
-            .then(response => response.json())
-            .then(data => setInstructionGroups(data))
-            .catch(error => console.error('Error fetching instruction groups', error));
-    }, []);
-
 
     return (
         <>
@@ -76,11 +61,15 @@ const AdminRecipes = () => {
                     <Link to="/admin/recipes?tab=delete" onClick={() => setActiveTab('delete')} className={activeTab === 'delete' ? 'active-tab btn btn-reset btn-tab margin-r-24' : 'btn btn-reset btn-tab margin-r-24'}>
                         Delete
                     </Link>
+                    <Link to="/admin/recipes?tab=ingredient" onClick={() => setActiveTab('ingredient')} className={activeTab === 'ingredient' ? 'active-tab btn btn-reset btn-tab margin-r-24' : 'btn btn-reset btn-tab margin-r-24'}>
+                        Ingredients
+                    </Link>
                 </div>
             </div>
             {activeTab === 'add' && <AdminRecipeAdd smallwares={smallwares} recipes={recipes} ingredients={ingredients} />}
             {activeTab === 'edit' && <AdminRecipeEdit smallwares={smallwares} recipes={recipes} ingredients={ingredients} />}
-            {activeTab === 'delete' && <AdminRecipeDelete recipes={recipes} ingredients={ingredients} recipeIngredients={recipeIngredients} />}
+            {activeTab === 'delete' && <AdminRecipeDelete smallwares={smallwares} recipes={recipes} ingredients={ingredients} recipeIngredients={recipeIngredients} />}
+            {activeTab === 'ingredient' && <AdminRecipeIngredient ingredients={ingredients} setIngredients={setIngredients} />}
         </>
     );
 };
