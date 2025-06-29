266cf2b538e74693b4dd285a1a5ad9c7e5a6aa1c
zaklance <wosewick@gmail.com>
2025-06-29 10:51:58 -0400

    delete unused file

--- client/src/utils/downloadPDF.js
+++
@@ -1,7 +0,0 @@
-import { pdf } from '@react-pdf/renderer';
-import { saveAs } from 'file-saver';
-
-export async function generateAndDownloadPDF(document, fileName) {
-    const blob = await pdf(document).toBlob();
-    saveAs(blob, fileName);
-}
